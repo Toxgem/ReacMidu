f112257b9dc67a4eb423a1af158998a5bb5d5077
Toxgem <DarkSek@gmail.com>
2023-11-24 00:58:53 -0400

    up2img

--- React1/06-NovelaGraficaVal/Dedicatoria a Val/src/App.jsx
+++ React1/06-NovelaGraficaVal/Dedicatoria a Val/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
       const [messages, setMessages] = useState(['Esto es para ti']);
     
       const [count, setCount] = useState(0);
-      const imagenes = ["./src/assets/valestilo5.jpg", "./src/assets/valestilo4.jpg", "./src/assets/ValEstilo2.jpeg",
-    "./src/assets/ValEstilo1.jpeg", `./src/assets/MarcoProf.png`];
+      const imagenes = ["/src/assets/valestilo5.jpg", "/src/assets/valestilo4.jpg", "/src/assets/ValEstilo2.jpeg",
+    "/src/assets/ValEstilo1.jpeg", `/src/assets/MarcoProf.png`];
 
 
       const mensajesParaVal= ["Probablemente ni tu ni yo esperabamos que esta tradicion durara tanto tiempo",
